e7ae2b22489c7003843e1a088850b3eb5a42f7e6
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-13 11:54:38 +0530

    Grains exercise finished

---
+++ Python/grains.py
@@ -0,0 +1,11 @@
+def square(number):
+    if number > 64 or number <= 0:
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number - 1)
+
+
+def total():
+    total = 0
+    for i in range(1,65):
+        total += square(i)
+    return total
